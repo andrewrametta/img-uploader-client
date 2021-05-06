e2fd9b778526e10ff18d254724b5276f81d7b2e7
Andrew <ramettaandrewj@gmail.com>
2021-05-06 10:14:51 -0400

    added upload functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [fileInputState, setFileInputState] = useState("");
   const [selectedFile, setSelectedFile] = useState("");
   const [previewSource, setPreviewSource] = useState("");
+  const [imgName, setImgName] = useState("");
+  const [imgurl, setImgurl] = useState("");
 
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
@@ -23,12 +25,39 @@ function App() {
   const handleSubmitFile = (e) => {
     e.preventDefault();
     if (!previewSource) return;
-    uploadImg(previewSource);
+    fetchImgJSON(previewSource).then((img) => {
+      console.log(img);
+      setImgurl(img.secure_url);
+    });
   };
 
-  const uploadImg = (base64EncodedImage) => {
-    console.log(base64EncodedImage);
+  const handleNameChange = (e) => {
+    const imgName = e.target.value;
+    setImgName(imgName);
   };
+
+  // const uploadImg = async (base64EncodedImage) => {
+  //   console.log(base64EncodedImage);
+  //   try {
+  //     const response = await fetch("http://localhost:8000/api/upload", {
+  //       method: "POST",
+  //       body: JSON.stringify({ data: (base64EncodedImage) }),
+  //       headers: { "Content-type": "application/json" },
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  async function fetchImgJSON(base64EncodedImage) {
+    const response = await fetch("http://localhost:8000/api/upload", {
+      method: "POST",
+      body: JSON.stringify({ data: base64EncodedImage }),
+      headers: { "Content-type": "application/json" },
+    });
+    const itemImg = await response.json();
+    return itemImg;
+  }
   return (
     <div className="App">
       <h1>Pretty Please work</h1>
@@ -40,13 +69,23 @@ function App() {
           value={fileInputState}
           className="form-input"
         />
+
+        <div>
+          {previewSource && (
+            <img
+              src={previewSource}
+              alt="chosen-img"
+              style={{ height: "300px" }}
+            />
+          )}
+        </div>
+        <label htmlFor="img-name">Name your image</label>
+        <input type="text" name="imgName" onChange={handleNameChange} />
         <button className="form-btn" type="submit">
           Submit
         </button>
       </form>
-      {previewSource && (
-        <img src={previewSource} alt="chosen-img" style={{ height: "300px" }} />
-      )}
+      <img src="http://res.cloudinary.com/dj4cj4ori/image/upload/v1620161195/payitforward-items/kfmdp3xzgfk3ne5xtzwd.png" />
     </div>
   );
 }
